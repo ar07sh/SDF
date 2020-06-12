63675fe6857157873918d06b3f246b07dd9d290d
ar07sh <66336122+ar07sh@users.noreply.github.com>
2020-06-12 10:56:41 -0400

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,46 @@
 package pickacard;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
- *
- * @author srinivsi
- * @author Paul Bonenfant
+import java.util.*;
+
+/*
+* @author srinivsi
+* @author arsh
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
 
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        Scanner arsh = new Scanner(System.in);
+        System.out.println("Enter a card suit: ");
+        String cardSuit = arsh.nextLine();
+        System.out.println("Enter a card value: ");
+        int cardValue = arsh.nextInt();
+
+        Card[] trick = new Card[7];
+        int i;
+        boolean match = false;
+
+        for (i = 0; i < trick.length; i++) {
+
+            Card mearsh = new Card();
+            mearsh.setValue(mearsh.randomValue());
+            mearsh.setSuit(Card.Suit[mearsh.randomSuit()]);
+            trick[i] = mearsh;
+
+            if (cardValue == trick[i].getValue()) {
+                System.out.println("Your Card Found");
+                match = true;
+            }
+
+            if (match == false) {
+                System.out.println("Not Found");
+                System.out.println("You Lost");
+            } else {
+                System.out.println("You Won !");//win
+            }
+
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
     }
 
 }
